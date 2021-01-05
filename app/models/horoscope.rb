646d53bf552842b9c8af80a218c5f67bbffcbe3c
sdelane2 <seanmartindelaney@gmail.com>
2021-01-05 17:22:00 -0500

    user id uniqueness

--- app/models/horoscope.rb
+++ app/models/horoscope.rb
@@ -1,4 +1,6 @@
 class Horoscope < ApplicationRecord
     has_many :favorite_horoscopes
     has_many :users, through: :favorite_horoscope
+
+    # validates :description, uniqueness: { scope: :current_date }
 end
